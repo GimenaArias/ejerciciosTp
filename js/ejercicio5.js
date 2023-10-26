1e2cac0c9f60b22755a2335302eb7016e368effe
GimenaArias <gime.arias.az@gmail.com>
2023-10-26 09:01:58 -0300

    agrego el ejercicio 5

---
+++ js/ejercicio5.js
@@ -0,0 +1,3 @@
+const numero1 = parseInt(prompt("Por favor, introduce el primer número:"));
+const numero2 = parseInt(prompt("Introduce el segundo número:"));
+document.write('La suma de los dos números es: ', numero1 + numero2);
